d8cad3d706236b7007e6c199ce718054594e1d3e
Jonathan Moore <moore@eds.org>
2024-05-19 22:26:18 -0700

    got sleep waiting for intrupt working

--- m8.c
+++ m8.c
@@ -6,6 +6,7 @@
 #define BUZZER 4
 
 // GPIO C
+#define SWICH 0
 #define RED_LED 1
 #define GREEN_LED 2
 #define BLUE_LED 3
@@ -14,15 +15,40 @@ int main()
 {
 	SystemInit();
 
+	// This delay gives us some time to reprogram the device. 
+	// Otherwise if the device enters standby mode we can't 
+	// program it any more.
+	Delay_Ms(5000);
+
 	// Enable GPIOs
 	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC;
 
+	
 	GPIO_CONFIG_OUT_PP(C, RED_LED, GREEN_LED, BLUE_LED);
 	GPIO_CONFIG_OUT_PP(D, BOARD_LED, BUZZER);
 
+	GPIO_CONFIG_ONE_IN_PUPD(C, SWICH);
+	GPIOC->BSHR |= GPIO_BSHR_BS0;
 
+	// AFIO is needed for EXTI
+	RCC->APB2PCENR |= RCC_AFIOEN;
+
+	// assign pin 0 interrupt from portC (0b10) to EXTI channel 0
+	AFIO->EXTICR |= (uint32_t)(0b10 << (2*SWICH));
+
+	// enable line2 interrupt event
+	EXTI->EVENR |= EXTI_Line0;
+	EXTI->FTENR |= EXTI_Line0;
+
+	// select standby on power-down
+	PWR->CTLR |= PWR_CTLR_PDDS;
+
+	// peripheral interrupt controller send to deep sleep
+	PFIC->SCTLR |= (1 << 2);
+	
 	while(1)
 	{
+
 		SET_GPIO(D, ON(BOARD_LED));
 
 		SET_GPIO(C, ON(RED_LED)|OFF(GREEN_LED)|OFF(BLUE_LED));
@@ -34,10 +60,11 @@ int main()
 		SET_GPIO(C, OFF(RED_LED)|OFF(GREEN_LED)|ON(BLUE_LED));
 		Delay_Ms(250);
 
-		SET_GPIO(D, OFF(BOARD_LED));
+		//SET_GPIO(D, OFF(BOARD_LED));
 		SET_GPIO(C, OFF(RED_LED)|OFF(GREEN_LED)|OFF(BLUE_LED));
 
-		Delay_Ms(1000);
+		__WFE();
+		SystemInit(); // Reset clocks
 	}
 }
 
