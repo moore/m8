9c21d2406b786aac00f355bb8aa3e400082eb71c
Jonathan Moore <moore@eds.org>
2024-06-08 22:04:00 -0700

    got fancy anamated lights started

--- blink.c
+++ blink.c
@@ -1,41 +1,115 @@
 #include "ch32v003fun.h"
 #include <stdio.h>
 
+uint32_t hz_to_ticks(uint32_t hz) {
+	return (1000 * DELAY_MS_TIME) / hz;
+}
+
+int sample_square_wave(uint32_t ticks, uint32_t lambda) {
+	return ((ticks/(lambda/2)) & 1) == 1;
+}
+
+int sample_pwm(uint32_t ticks, uint32_t hz, uint32_t duty_ticks) {
+	return (ticks % hz) <= duty_ticks;
+}
+
+uint32_t compute_freq(
+	uint32_t now,
+	uint32_t duration,
+	uint32_t start_time,
+	uint32_t end_time, 
+	uint32_t start_hz, 
+	uint32_t end_hz
+	) {
+		if (now >= end_time ) {
+			return end_hz;
+		}
+
+		uint16_t change = abs(end_hz - start_hz);
+		float percent = ((float)(now - start_time))/((float)duration);
+		uint32_t delta = (uint32_t)(change * percent);
+
+		if (start_hz < end_hz) {
+			return start_hz + delta;
+		} else {
+			return start_hz - delta;
+		}
+}
+
+void drive( int pin, uint32_t start_hz, uint32_t end_hz, uint32_t duration_ms) {
+	uint32_t duration_ticks = duration_ms * DELAY_MS_TIME;
+	uint32_t start = SysTick->CNT;
+	uint32_t end = start + duration_ticks;
+	uint32_t now = start;
+
+	while ((now - start) < (DELAY_MS_TIME * duration_ms))  {
+
+		uint32_t hz = compute_freq(now, duration_ticks, start, end, start_hz, end_hz);
+		uint32_t freq = hz_to_ticks(hz);
+
+		if ( sample_square_wave(now, freq) ) {
+			funDigitalWrite( pin, FUN_HIGH );
+		} else {
+			funDigitalWrite( pin, FUN_LOW );
+		}
+
+		now = SysTick->CNT;
+	}
+
+	funDigitalWrite(pin, FUN_LOW);
+}
+
+
+void drive_pwm( int pin, uint32_t freq,  uint32_t start_duty, uint32_t end_duty, uint32_t duration_ms) {
+	uint32_t duration_ticks = duration_ms * DELAY_MS_TIME;
+	uint32_t start = SysTick->CNT;
+	uint32_t end = start + duration_ticks;
+	uint32_t now = start;
+
+	while ((now - start) < (DELAY_MS_TIME * duration_ms))  {
+
+		uint32_t duty = compute_freq(now, duration_ticks, start, end, start_duty, end_duty);
+
+
+		if (sample_pwm(now, freq, duty)) {
+			funDigitalWrite(pin, FUN_HIGH);
+		} else {
+			funDigitalWrite(pin, FUN_LOW);
+		}
+
+		now = SysTick->CNT;
+	}
+
+	funDigitalWrite(pin, FUN_LOW);
+}
+
+
 int main()
 {
 	SystemInit();
 
-	// This delay gives us some time to reprogram the device. 
-	// Otherwise if the device enters standby mode we can't 
-	// program it any more.
-	//Delay_Ms(5000);
-
-
 	// Enable GPIOs
 	funGpioInitAll();
 	
-	//funPinMode( PD0, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
-	//funPinMode( PD4, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
-	//funPinMode( PD6, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
-	//funPinMode( PC0, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
-    funPinMode( PD2, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
-
+    funPinMode( PC1, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PC2, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+    funPinMode( PC3, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( PD4, GPIO_Speed_50MHz | GPIO_CNF_OUT_PP );
 
 	while(1)
 	{
-		//funDigitalWrite( PD0, FUN_HIGH );
-		//funDigitalWrite( PD4, FUN_HIGH );
-		//funDigitalWrite( PD6, FUN_HIGH );
-		//funDigitalWrite( PC0, FUN_HIGH );
-		funDigitalWrite( PD2, FUN_HIGH );
+		
+		drive_pwm(PC1, 10000, 100, 8000, 1000);
+		drive_pwm(PC1, 10000, 8000, 100, 1000);
 
-		Delay_Ms( 250 );
-		//funDigitalWrite( PD0, FUN_LOW );
-		//funDigitalWrite( PD4, FUN_LOW );
-		//funDigitalWrite( PD6, FUN_LOW );
-		//funDigitalWrite( PC0, FUN_LOW );
-                funDigitalWrite( PD2, FUN_LOW );
+		drive_pwm(PC2, 10000, 100, 8000, 1000);
+		drive_pwm(PC2, 10000, 8000, 100, 1000);
 
-		Delay_Ms( 250 );
+		drive_pwm(PC3, 10000, 100, 8000, 1000);
+		drive_pwm(PC3, 10000, 9000, 100, 1000);
+
+
+		//drive(PD4, 1915, 1915, 1000);
+		
 	}
 }
--- m8.c
+++ m8.c
@@ -1,8 +1,8 @@
 #include "ch32v003fun.h"
+#include <stdlib.h>
 #include "m8.h"
 
 // GPIO D
-#define BOARD_LED 2
 #define BUZZER 4
 
 // GPIO C
@@ -11,10 +11,124 @@
 #define GREEN_LED 2
 #define BLUE_LED 3
 
+#define LED_FREQ 10000
+
+typedef struct led_state_t
+{
+	uint8_t on;
+	uint16_t start_value;
+	uint16_t end_value;
+	uint32_t start_ticks;
+	uint32_t duration_ticks;
+} LedState;
+
+
 typedef struct state_t {
 	uint32_t health;
 	uint32_t last_decay;
+
+	LedState red;
+	LedState green;
+	LedState blue;
 } State;
+
+
+uint32_t ms_to_ticks(uint32_t ms) {
+	return ms * DELAY_MS_TIME;
+}
+
+uint32_t hz_to_ticks(uint32_t hz) {
+	return (1000 * DELAY_MS_TIME) / hz;
+}
+
+int sample_pwm(uint32_t ticks, uint32_t hz, uint32_t duty_ticks) {
+	return (ticks % hz) <= duty_ticks;
+}
+
+uint32_t compute_value(
+	uint32_t now_ticks,
+	uint32_t duration_ticks,
+	uint32_t start_ticks,
+	uint32_t start_value, 
+	uint32_t end_value
+	) {
+		uint32_t end_ticks = start_ticks + duration_ticks;
+
+		if (now_ticks >= end_ticks ) {
+			return end_value;
+		}
+
+		uint16_t change = abs(end_value - start_value);
+		float percent = ((float)(now_ticks - start_ticks))/((float)duration_ticks);
+		uint32_t delta = (uint32_t)(change * percent);
+
+		if (start_value < end_value) {
+			return start_value + delta;
+		} else {
+			return start_value - delta;
+		}
+}
+
+
+int check_led(int pin, uint32_t now, LedState* led, int* done) {
+
+	if (!led->on) {
+		return OFF(pin);
+	}
+
+	if (now > (led->start_ticks + led->duration_ticks)) {
+		led->on = 0;
+		return OFF(pin);
+	}
+
+	*done = 0;
+
+	if (now < led->start_ticks) {
+		return OFF(pin); 
+	}
+
+	uint32_t duty = compute_value(
+		now,
+		led->duration_ticks,
+		led->start_ticks,
+		led->start_value,
+		led->end_value);
+
+
+	if (sample_pwm(now, LED_FREQ, duty)) {
+		return ON(pin);
+	} else {
+		return OFF(pin);
+	}
+}
+
+int update_state(State* state) {
+	uint32_t now = SysTick->CNT;
+	int done = 1;
+	int gpio_c = check_led(RED_LED, now, &state->red, &done)
+	           | check_led(GREEN_LED, now, &state->green, &done)
+	           | check_led(BLUE_LED, now, &state->blue, &done);
+
+	SET_GPIO(C, gpio_c);
+
+	return done;
+}
+
+
+void set_envelope(
+	LedState* led,
+	uint32_t now,
+	uint32_t delay_ms,
+	uint32_t duration_ms, 
+	uint32_t start_value,
+	uint32_t end_value
+	) {
+		led->on = 1;
+		led->start_ticks = now + ms_to_ticks(delay_ms);
+		led->duration_ticks = ms_to_ticks(duration_ms);
+		led->start_value = start_value;
+		led->end_value = end_value;
+}
 
 int main()
 {
@@ -25,7 +139,8 @@ int main()
 	// program it any more.
 	Delay_Ms(5000);
 
-	State state = {0, SysTick->CNT};
+	State state = {};
+	state.last_decay = SysTick->CNT;
 
 	// Enable GPIOs
 	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC;
@@ -34,6 +149,19 @@ int main()
 	GPIO_CONFIG_OUT_PP(A, 0, 1, 2, 3, 4, 5, 6, 7);
 	GPIO_CONFIG_OUT_PP(C,    RED_LED, GREEN_LED, BLUE_LED, 4, 5, 6, 7);
 	GPIO_CONFIG_OUT_PP(D, 0, 1, 2, 3, BUZZER, 5, 6, 7);
+
+
+	SET_GPIO(C, ON(RED_LED)|OFF(GREEN_LED)|OFF(BLUE_LED));
+	Delay_Ms(250);
+
+	SET_GPIO(C, OFF(RED_LED)|ON(GREEN_LED)|OFF(BLUE_LED));
+	Delay_Ms(250);
+
+	SET_GPIO(C, OFF(RED_LED)|OFF(GREEN_LED)|ON(BLUE_LED));
+	Delay_Ms(250);
+
+	SET_GPIO(C, OFF(RED_LED)|OFF(GREEN_LED)|OFF(BLUE_LED));
+
 
 
 	GPIO_CONFIG_ONE_IN_PUPD(C, SWICH);
@@ -64,19 +192,18 @@ int main()
 	
 	while(1)
 	{
+		
+		uint32_t now = SysTick->CNT;
+		set_envelope(&state.red,   now,   0, 1000, 100, 8000);
+		set_envelope(&state.green, now, 333,  666, 100, 8000);
+		set_envelope(&state.blue,  now, 666,  333, 100, 8000);
 
-		SET_GPIO(D, ON(BOARD_LED));
-
-		SET_GPIO(C, ON(RED_LED)|OFF(GREEN_LED)|OFF(BLUE_LED));
-		Delay_Ms(250);
-
-		SET_GPIO(C, OFF(RED_LED)|ON(GREEN_LED)|OFF(BLUE_LED));
-		Delay_Ms(250);
-
-		SET_GPIO(C, OFF(RED_LED)|OFF(GREEN_LED)|ON(BLUE_LED));
-		Delay_Ms(250);
-
-		//SET_GPIO(D, OFF(BOARD_LED));
+		int done = 0;
+		while (!done)
+		{
+			done = update_state(&state);
+		}
+		
 		SET_GPIO(C, OFF(RED_LED)|OFF(GREEN_LED)|OFF(BLUE_LED));
 
 		__WFE();
